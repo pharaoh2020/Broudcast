416e51e260afc33884ebf7af1869014822080bb9
pharaoh2020 <47272829+pharaoh2020@users.noreply.github.com>
2019-02-02 16:07:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,31 +14,3 @@ client.on('message', msg => {
 
 client.login(process.env.BOT_TOKEN);  //لا تحط التوكن حقك هنا
 
-client.on("message", message => {
-
-  if (message.content.startsWith(prefix + "bc")) {
-               if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-let args = message.content.split(" ").slice(1);
-var argresult = args.join(' '); 
-message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
-m.send(${argresult}\n ${m});
-})
-message.channel.send(${message.guild.members.filter(m => m.presence.status !== 'all').size}: عدد الاعضاء المستلمين); 
-message.delete(); 
-};
-})
-
-client.on('ready',  () => {
-  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
-  console.log('by DG');
-  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-  console.log(`Logged in as  * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
-  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-  console.log('is online')
-client.user.setStatus("dnd");
-});
-
-// playing
-client.on('ready', () => {                           
-client.user.setGame(`DG-Welcome`,'https://www.twitch.tv/tarikrs');                                                                                                                                                                                                                                                                                                                                                                                                                             client.user.setGame(`DG-Welcome`,'https://www.twitch.tv/v5bz');
-});
