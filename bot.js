44081865b758057679ea02cdafaad68290e9aa8e
pharaoh2020 <47272829+pharaoh2020@users.noreply.github.com>
2019-02-02 15:59:49 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "$"
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login(process.env.BOT_TOKEN);  //لا تحط التوكن حقك هنا
+
+client.on("message", message => {
+
+  if (message.content.startsWith(prefix + "bc")) {
+               if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+let args = message.content.split(" ").slice(1);
+var argresult = args.join(' '); 
+message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+m.send(${argresult}\n ${m});
+})
+message.channel.send(${message.guild.members.filter(m => m.presence.status !== 'all').size}: عدد الاعضاء المستلمين); 
+message.delete(); 
+};
+})
